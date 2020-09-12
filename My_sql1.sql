ad4a890d51ab8297b2308967226151294a18f300
Lucky Abolorunke <62015433+Oneimu@users.noreply.github.com>
2020-09-12 16:42:46 +0100

    Add files via upload
    Database code for the number of covid19 cases in 6 regions across the global

---
+++ My_sql1.sql
@@ -0,0 +1,32 @@
+use `lucky`;
+
+create table  `Situation_report`(
+ `S/N` INT(72) NOT NULL AUTO_INCREMENT,
+ `Date` varchar(30),
+  `Confirmed cases` int(30),
+   `Death` int(30),
+   `Recovered Cases` int(30),
+   primary key (`S/N`)
+);
+insert into `Situation_report` Values (1,'MAR1',87137,2977, 42700);
+insert into `Situation_report` Values (2,'MAR2',88948, 3043,45200   );
+insert into `Situation_report` Values (3,'MAR3',90869, 3112,  48200 );
+insert into `Situation_report` Values (4,'MAR4',93091, 3198,51200 );
+insert into `Situation_report` Values (5,'MAR5',95324, 3281,53800   );
+insert into `Situation_report` Values (6,'MAR6',98192, 3380,55900   );
+insert into `Situation_report` Values (7,'MAR7',101927, 3486,  58400 );
+insert into `Situation_report` Values (8,'MAR8',105586, 3584,  60700 );
+insert into `Situation_report` Values (9,'MAR9',109577, 3809, 62500  );
+insert into `Situation_report` Values (10,'MAR10',113702,4012 , 64400  );
+insert into `Situation_report` Values (11,'MAR11',118319, 4292, 67000  );
+insert into `Situation_report` Values (12,'MAR12',125260, 4613, 68300  );
+insert into `Situation_report` Values (13,'MAR13',132758, 4955,  70200 );
+insert into `Situation_report` Values (14,'MAR14',142539, 5393, 72600  );
+insert into `Situation_report` Values (15,'MAR15',153517, 5735, 75100  );
+insert into `Situation_report` Values (16,'MAR16',169387, 6513,77300   )
+
+
+
+
+
+
